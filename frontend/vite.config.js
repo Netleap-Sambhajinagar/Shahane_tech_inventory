20fa812ab70d54bc60e379975a90b6d20e4dfb1e
Dnyaneshwar More <dnyaneshwarmore622@gmail.com>
2026-03-24 10:30:39 +0530

    Fix Vercel routing and remove base path from Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
   resolve: {
     alias: {
       '@admin': path.resolve(__dirname, './src/components/Admin'),
